2082e0c7c281673181a13302a881349f5d932548
Sander <sanderpeguero.21@gmail.com>
2022-05-25 22:19:03 -0400

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Components/Navbar'
+import Navbar2 from './Components/Navbar2'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Navbar2/>
+     {/* <div>Hola Mundo</div> */}
+    </>
+  )
+}
+
+export default App
