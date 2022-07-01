b87068fe67bfeff2c17074bc218d018bc9737674
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-07-01 15:05:06 -0400

    Polify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { outlinedInputClasses } from '@mui/material'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +14,11 @@ export default defineConfig({
             global: 'globalThis'
         },
         // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                buffer: true
-            })
-        ]
+        // plugins: [
+        //     NodeGlobalsPolyfillPlugin({
+        //         buffer: true
+        //     })
+        // ]
     }
   }
 })
