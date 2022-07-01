acc273a9c63944207c167c47d73724f85c90475d
Albert Mendoza <albertmendoza0462@hotmail.com>
2022-07-01 12:10:59 -0400

    page of product information

--- src/Components/Store/InfoProducto/InfoProducto.jsx
+++ src/Components/Store/InfoProducto/InfoProducto.jsx
@@ -1,17 +1,23 @@
+import { width } from '@mui/system';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { indigo } from '@mui/material/colors';
 import { useState, useEffect } from 'react';
 import { useQuery } from '../../../Hooks/useQuery';
+import styles from './InfoProducto.module.css'
 
 
-const InfoProducto = ({ products }) => {
-    
+
+const InfoProducto = ({ products, shoppingCart }) => {
     const [isLoading, setIsLoading] = useState(true);
-    
+
     const [Product, setProduct] = useState(null);
     const query = useQuery();
     const search = query.get("search");
 
-    useEffect(() => {  
-        
+    useEffect(() => {
+
         const data = products.find((d) => d.IDProducto == search);
         console.log(search)
 
@@ -28,25 +34,69 @@ const InfoProducto = ({ products }) => {
     if (!Product) {
         return null;
     }
-    
+
+    const ColorButton = styled(Button)(({ theme }) => ({
+        color: theme.palette.getContrastText(indigo[800]),
+        backgroundColor: indigo[800],
+        '&:hover': {
+            backgroundColor: indigo[700],
+        },
+    }));
+
     return (
         <>
-            <img src={Product.QRCode} style={{ height: '100%', width: '100%' }} />
-            
-            <div style={{
-                position: 'absolute',
-                top: '50%',
-                left: '27%',
-                transform: 'translate(-50%, -50%)',
-                color: 'black'
-            }}>
+            <div className={styles.background}>
+            <article className={styles.article} >
+
                 <div style={{
-                    fontSize: '60px',
-                    fontWeight: '700',
-                    fontFamily: 'sans-serif'
+                    maxWidth: 500,
+                    margin: 20
                 }}>
-                    {Product.Descripcion}
+                    <img src={Product.QRCode} style={{
+                        width: '100%'
+                    }} />
                 </div>
+
+                <div style={{
+                    margin: 20,
+                    Width: 600,
+                    wordWrap: 'break-word'
+                }}>
+
+                    <h1 className={styles.h1} >
+                        {Product.Descripcion}
+                    </h1>
+                    <hr className={styles.hr} />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className={styles.td} >Precio:</td>
+                                <td>{Product.Precio}</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.td} >Descuento:</td>
+                                <td>{Product.Descuento}%</td>
+                            </tr>
+                            <tr>
+                                <td className={styles.td} >Cantidad Restante:</td>
+                                <td>{Product.CantidadRestante}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    {/* <ColorButton variant="outlined" style={{
+                        marginLeft: '1rem',
+                        justifySelf: 'u'
+                    }}
+                        onClick={() => {
+                            shoppingCart.push(product)
+                        }}>
+                        {"   "}
+                        Add    <AddShoppingCartIcon fontSize="small" style={{ marginLeft: '0.50rem' }} />
+                    </ColorButton> */}
+                </div>
+
+            </article>
             </div>
         </>
 
