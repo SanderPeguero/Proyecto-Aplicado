fb87d99841a6278cce6c1c68305eeaec736cc8f4
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-07-01 22:45:38 -0400

    .

--- src/Components/Wallet/Data/Data.jsx
+++ src/Components/Wallet/Data/Data.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { loadAccount } from '../util/LoadAccount'
+import { loadAccount } from '../utils/LoadAccount'
 import Styles from './Data.module.css'
 import Chip from '../../../Images/Chip.png'
 import Button from '@mui/material/Button';
