95684f7614a8b7df200b533561686422722a0a44
Sander Peguero <sanderpeguero.21@gmail.com>
2022-07-05 17:54:35 -0400

    Wallet

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
-  // ,
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  // //       // Enable esbuild polyfill plugins
-  //       plugins: [
-  //           NodeGlobalsPolyfillPlugin({
-  //               buffer: true
-  //           })
-  //       ]
-  //   }
-  // },
+  ,
+  optimizeDeps: {
+    esbuildOptions: {
+  //       // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                buffer: true
+            })
+        ]
+    }
+  },
   // build: {
   //   esbuildOptions: {
   //     //       // Enable esbuild polyfill plugins
